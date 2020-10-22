52fcc8ff066b1281201182f92f2f61af9b1246d8
Gama Tellez <gamatellez@gmail.com>
2020-10-22 12:09:36 -0600

    Chnage bright green color to a more sublte green

--- ApproachableGeekChallenge/FormVC/ValidationLabel.swift
+++ ApproachableGeekChallenge/FormVC/ValidationLabel.swift
@@ -10,13 +10,21 @@ import UIKit
 class ValidationLabel: UILabel {
     private var infoType : ProfileInfo?
     private var stringToCheck : String?
+    private var valid : Bool {
+        didSet {
+            self.textColor = self.valid == true ? UIColor(red: 0, green: 0.7373, blue: 0.1725, alpha: 1.0) : UIColor.red
+        }
+    }
     
     override init(frame: CGRect) {
+        self.valid = false
         super.init(frame: frame)
         self.translatesAutoresizingMaskIntoConstraints = false
-        self.font = self.font.withSize(10)
+        self.font = self.font.withSize(13)
         self.numberOfLines = 0
+        self.lineBreakMode = .byWordWrapping
         self.textAlignment = .center
+        self.adjustsFontSizeToFitWidth = true
     }
     
     required init?(coder: NSCoder) {
@@ -24,7 +32,7 @@ class ValidationLabel: UILabel {
     }
     
     
-    internal func validateInput(inputOne: String?, inputTwo: String?, for type:ProfileInfo) -> Bool {
+    internal func isInputValidated(inputOne: String?, inputTwo: String?, for type:ProfileInfo) -> Bool {
         switch type {
         case .name:
             return self.validateName(firstName: inputOne, lastName: inputTwo)
@@ -39,28 +47,28 @@ class ValidationLabel: UILabel {
     
     private func validateName(firstName: String?, lastName: String?) -> Bool {
         guard let fName = firstName, let lName = lastName else {
-            self.updateText(message: "Oops, either your first or last name are missing...")
+            self.update(message: "Oops, either your first or last name are missing.", valid: false)
             return false
         }
         if !self.containsWhites(text: fName) && !self.containsWhites(text: lName) {
-            self.updateText(message: "Great, we are good to go!")
+            self.update(message: "Great, we are good to go!", valid: true)
             return true
         } else {
-            self.updateText(message: "Oops, either your first or last name are missing...")
+            self.update(message: "Oops, either your first or last name is missing.", valid: false)
             return false
         }
     }
     
     private func validateEmail(email: String?) -> Bool {
         guard let eMail = email else {
-            self.updateText(message: "Oops, there is something wrong with your email!")
+            self.update(message: "Oops, there is something wrong with your email!", valid: false)
             return false
         }
         if self.isEmailValid(email: eMail) {
-            self.updateText(message: "Sweet!, looks good to me!")
+            self.update(message: "Sweet!!", valid: true)
             return true
         } else {
-            self.updateText(message: "Oops, there is something wrong with your email!")
+            self.update(message: "Oops, there is something wrong with your email!", valid: false)
             return false
         }
         
@@ -68,39 +76,40 @@ class ValidationLabel: UILabel {
     
     private func validatePhone(phone: String?) -> Bool {
         guard let pNumber = phone else {
-            self.updateText(message: "Oops, could you check the number again?")
+            self.update(message: "Oops, could you check the number again?", valid: false)
             return false
         }
         if pNumber.isEmpty {
-            self.updateText(message: "We really need your number!")
+            self.update(message: "We really need your number!", valid: false)
             return false
         }
         if isPhoneNumberValid(phone: pNumber) {
-            self.updateText(message: "It looks right to us!")
+            self.update(message: "It looks good!", valid: true)
             return true
         } else {
-            self.updateText(message: "Oops, could you check the number again?")
+            self.update(message: "Oops, could you check the number again?", valid: false)
             return false
         }
     }
     
     private func validateAbout(about: String?) -> Bool {
         guard let intro = about else {
-            self.updateText(message: "Oops, make sure to write something about your self!")
+            self.update(message: "Oops, make sure to write something about your self!", valid: false)
             return false
         }
         if !self.containsWhites(text: intro) {
-            self.updateText(message: "Awesome, thank you for sharing a little about you!")
+            self.update(message: "Awesome, thank you for sharing a little about you!", valid: true)
             return true
         } else {
-            self.updateText(message: "Oops, we really want to know a little about you!")
+            self.update(message: "Oops, we really want to know a little about you!", valid: false)
             return false
         }
     }
     
-    private func updateText(message: String) {
-        UIView.transition(with: self, duration: 0.5, options: .transitionCrossDissolve, animations: {
+    private func update(message: String, valid: Bool) {
+        UIView.transition(with: self, duration: 0.1, options: .transitionCrossDissolve, animations: {
             self.text = message
+            self.valid = valid
         }, completion: nil)
     }
     
@@ -122,7 +131,7 @@ class ValidationLabel: UILabel {
     }
     
     internal func clear() {
-        self.updateText(message: "")
+        self.update(message: "", valid: false)
     }
 }
 

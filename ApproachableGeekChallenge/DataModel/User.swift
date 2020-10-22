893dd0794e9ac30464da9939048b8af8a162c9ce
Gama Tellez <gamatellez@gmail.com>
2020-10-22 12:04:51 -0600

    Modified class membes to work wit app architechture

--- ApproachableGeekChallenge/DataModel/User.swift
+++ ApproachableGeekChallenge/DataModel/User.swift
@@ -9,18 +9,29 @@ import Foundation
 import UIKit
 
 class User {
-    private(set) var name : String?
+    private(set) var firstName : String?
+    private(set) var lastName : String?
     private(set) var phone : String?
     private(set) var email : String?
     private(set) var about : String?
     private(set) var profileImage : UIImage?
     
     init(name: String?, phone: String?, email: String?, intro: String?, profileImage: UIImage?) {
-        self.name = name
+        self.firstName = name
         self.phone = phone
         self.email = email
         self.about = intro
         self.profileImage = profileImage
     }
     
+    internal func updateInfo(infoType: ProfileInfo, inputOne: String?, inputTwo: String?) {
+        switch infoType {
+        case .name:
+            self.firstName = inputOne
+            self.lastName = inputTwo
+        case .phone: self.phone = inputOne
+        case .email: self.email = inputOne
+        case .about: self.about = inputOne
+        }
+    }
 }
---
+++ ApproachableGeekChallenge/FormVC/ActivityButton.swift
@@ -0,0 +1,20 @@
+//
+//  ActivityButton.swift
+//  ApproachableGeekChallenge
+//
+//  Created by Gamaliel Tellez on 10/22/20.
+//
+
+import UIKit
+
+class ActivityButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
